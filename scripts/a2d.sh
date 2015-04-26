be1ee091a78b926ace08b2167e5343a04a06d820
LightGuard <lightguard.jp@gmail.com>
2015-04-25 20:07:19 -0600

    Modifying the scripts for our use

--- scripts/a2d.sh
+++ scripts/a2d.sh
@@ -1,10 +1,10 @@
-#!/usr/bin
+#!/bin/sh
 
 # You must set BOOK_HOME to the home folder for your book project
 # BOOK_HOME=$HOME/Projects/BookProjects/walls.sia4
 
 MAKEPDF_HOME=$BOOK_HOME/AuthorInfo/AAMakePDF/
 
-asciidoctor -T $BOOK_HOME/asciidoc/templates -a chapterFilename=$1 -b docbook45 -o $BOOK_HOME/manuscript/$1.xml $1.txt
-awk -f $BOOK_HOME/asciidoc/templates/scripts/cofix.awk $BOOK_HOME/manuscript/$1.xml > $BOOK_HOME/manuscript/$1.fixed
-mv $BOOK_HOME/manuscript/$1.fixed $BOOK_HOME/manuscript/$1.xml
+asciidoctor -T $BOOK_HOME/asciidoc -a chapterFilename=$1 -b docbook45 -o $BOOK_HOME/Manuscript/$1.xml $1.adoc
+awk -f $BOOK_HOME/asciidoc/scripts/cofix.awk $BOOK_HOME/Manuscript/$1.xml > $BOOK_HOME/Manuscript/$1.fixed
+mv $BOOK_HOME/Manuscript/$1.fixed $BOOK_HOME/Manuscript/$1.xml
