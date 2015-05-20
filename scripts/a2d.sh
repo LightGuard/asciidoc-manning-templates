fbb817599e79f6f29f33969f581ec38f90cca8bb
LightGuard <lightguard.jp@gmail.com>
2015-05-20 00:16:19 -0600

    Fixing up scripts after the reorg

--- scripts/a2d.sh
+++ scripts/a2d.sh
@@ -3,8 +3,8 @@
 # You must set BOOK_HOME to the home folder for your book project
 # BOOK_HOME=$HOME/Projects/BookProjects/walls.sia4
 
-MAKEPDF_HOME=$BOOK_HOME/AuthorInfo/AAMakePDF/
+MAKEPDF_HOME=$BOOK_HOME/AAMakePDF/
 
-asciidoctor -T $BOOK_HOME/asciidoc -a chapterFilename=$1 -b docbook45 -o $BOOK_HOME/Manuscript/$1.xml $1.adoc
-awk -f $BOOK_HOME/asciidoc/scripts/cofix.awk $BOOK_HOME/Manuscript/$1.xml > $BOOK_HOME/Manuscript/$1.fixed
-mv $BOOK_HOME/Manuscript/$1.fixed $BOOK_HOME/Manuscript/$1.xml
+asciidoctor --trace -T $BOOK_HOME/asciidoc -a chapterFilename=$1 -b docbook45 -o $BOOK_HOME/docbook/$1.xml $1.adoc
+awk -f $BOOK_HOME/asciidoc/scripts/cofix.awk $BOOK_HOME/docbook/$1.xml > $BOOK_HOME/target/$1.fixed
+mv $BOOK_HOME/target/$1.fixed $BOOK_HOME/docbook/$1.xml
--- scripts/mpdf.sh
+++ scripts/mpdf.sh
@@ -3,11 +3,11 @@
 # You must set BOOK_HOME to the home folder for your book project
 # BOOK_HOME=/Users/habuma/Projects/BookProjects/walls.sia4
 
-MAKEPDF_HOME=$BOOK_HOME/AuthorInfo/AAMakePDF/
+MAKEPDF_HOME=$BOOK_HOME/AAMakePDF/
 
 pushd .
-cd $BOOK_HOME/Manuscript
+cd $BOOK_HOME
 mkdir -p $BOOK_HOME/target/pdf
-sh $MAKEPDF_HOME/AAMakePDF.sh $BOOK_HOME/Manuscript/$1.xml $BOOK_HOME/target/pdf/$1.pdf $MAKEPDF_HOME
-rm $BOOK_HOME/Manuscript/$1.xml.temp.xml
+sh $MAKEPDF_HOME/AAMakePDF.sh $BOOK_HOME/docbook/$1.xml $BOOK_HOME/target/pdf/$1.pdf $MAKEPDF_HOME
+rm $BOOK_HOME/docbook/$1.xml.temp.xml
 popd
