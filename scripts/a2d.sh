126edfe38cf1be6f65ef6079d7d52254903e3641
LightGuard <lightguard.jp@gmail.com>
2015-06-02 21:22:57 -0600

    No need for the awk scripts now

--- scripts/a2d.sh
+++ scripts/a2d.sh
@@ -6,5 +6,5 @@
 MAKEPDF_HOME=$BOOK_HOME/AAMakePDF/
 
 asciidoctor --trace -T $BOOK_HOME/asciidoc -a chapterFilename=$1 -b docbook45 -o $BOOK_HOME/docbook/$1.xml $1.adoc
-awk -f $BOOK_HOME/asciidoc/scripts/cofix.awk $BOOK_HOME/docbook/$1.xml > $BOOK_HOME/target/$1.fixed
-mv $BOOK_HOME/target/$1.fixed $BOOK_HOME/docbook/$1.xml
+#awk -f $BOOK_HOME/asciidoc/scripts/cofix.awk $BOOK_HOME/docbook/$1.xml > $BOOK_HOME/target/$1.fixed
+#mv $BOOK_HOME/target/$1.fixed $BOOK_HOME/docbook/$1.xml
